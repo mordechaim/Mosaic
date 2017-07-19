0d7a3857d0b9135b6dfa8a7a10c112a4e100b7e9
MouseEvent <mordechai112@gmail.com>
2017-07-19 13:10:54 -0400

    Added toString()

--- src/com/stackexchange/puzzling/user/mordechai/mosaic/Clue.java
+++ src/com/stackexchange/puzzling/user/mordechai/mosaic/Clue.java
@@ -7,25 +7,35 @@ public class Clue {
 	private boolean isPixel;
 
 	public Clue(Fill fill, int clue, boolean isPixel) {
-		this.fill = fill;
-		this.clue = clue;
-		this.isPixel = isPixel;
+		setFill(fill);
+		setClue(clue);
+		setIsPixel(isPixel);
 	}
 
 	public Clue(boolean isPixel) {
-		this.isPixel = isPixel;
+		setIsPixel(isPixel);
 	}
 
 	public Clue(Clue other) {
-		if (other == null)
+		if(other == null)
 			return;
 
-		this.fill = other.fill;
-		this.clue = other.clue;
-		this.isPixel = other.isPixel;
+		setFill(other.fill);
+		setClue(other.clue);
+		setIsPixel(other.isPixel);
 	}
 
 	public Clue() {
+	}
+
+	public Clue copy() {
+		return new Clue(this);
+	}
+
+	public void merge(Clue other) {
+		setClue(other.getClue());
+		setFill(other.getFill());
+		setIsPixel(other.isPixel());
 	}
 
 	public Fill getFill() {
@@ -52,4 +62,39 @@ public class Clue {
 		isPixel = b;
 	}
 
+	public String toString() {
+		return toString(true, true, true);
+	}
+
+	public String toString(boolean fill, boolean clue, boolean pixel) {
+
+		StringBuilder builder = new StringBuilder();
+		if (clue && getClue() >= 0)
+			builder.append(getClue());
+		if (fill)
+			builder.append(getFill());
+		if (pixel && isPixel())
+			builder.append("*");
+
+		return builder.toString();
+	}
+
+	public static Clue fromString(String str) {
+		Clue clue = new Clue();
+
+		clue.setIsPixel(str.contains("*"));
+		if (str.contains("#"))
+			clue.setFill(Fill.FILLED);
+		else if (str.contains("X"))
+			clue.setFill(Fill.X);
+
+		if (str.length() > 0) {
+			char ch = str.charAt(0);
+			if (Character.isDigit(ch)) {
+				clue.setClue(ch - '0');
+			}
+		}
+
+		return clue;
+	}
 }
